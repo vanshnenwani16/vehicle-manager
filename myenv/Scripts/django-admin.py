626a3d7d2755bf0723e48f51418a85610a109503
HET PATEL <patelhetit@gmail.com>
2024-02-29 18:22:14 +0530

    first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\HET PATEL\Downloads\Vehicle_Service_Using_Django_With_Source_Code\vehicle_service_management_django\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
