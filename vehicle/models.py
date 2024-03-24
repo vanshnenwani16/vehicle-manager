86443206daef50c1594895454759ce9e1e2c2e16
vanshnenwani16 <vanshnenwani1@gmail.com>
2024-03-24 15:09:16 +0530

    fixed admin

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class CustomAutoField(models.BigAutoField):
+    pass
+
 # Create your models here.
 class Customer(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
